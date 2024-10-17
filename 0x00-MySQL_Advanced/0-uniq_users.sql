feac359e9897b1b0651dee8c65e2ac38da15b838
MalekAjil <aboanasajil@gmail.com>
2024-10-17 00:58:46 +0000

    task0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- creates a table users following these requirements:
+-- With these attributes:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- If the table already exists, your script should not fail
+
