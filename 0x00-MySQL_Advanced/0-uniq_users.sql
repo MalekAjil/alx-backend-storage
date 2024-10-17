5b68d60a961ea06bb446b2b34e954bb8f680987c
MalekAjil <aboanasajil@gmail.com>
2024-10-17 22:36:30 +0000

    task0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -4,4 +4,6 @@
 -- email, string (255 characters), never null and unique
 -- name, string (255 characters)
 -- If the table already exists, your script should not fail
-
+CREATE TABLE IF NOT EXISTS users(id NUMBER NOT NULL PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255))
