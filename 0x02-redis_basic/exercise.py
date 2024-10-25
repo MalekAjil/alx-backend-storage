5f451475a254cf62d18a6ea5cce98a26f867e25b
MalekAjil <aboanasajil@gmail.com>
2024-10-25 14:22:17 -0700

    task0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" exercise """
+import redis
+
+
+calss Cache:
+    """Cache class"""
+    def __init__(self):
+        """initialize the object"""
+        _redis = redis.Redis()
+
