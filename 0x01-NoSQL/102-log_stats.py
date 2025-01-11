f2182e06676baa25124e6141e574c3ac30f9651a
MalekAjil <aboanasajil@gmail.com>
2025-01-11 10:38:05 -0800

    task15

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Log States Module"""
+
+
+from pymongo import MongoClient
+
+
+def log_stats(mongo_collection):
+    """
+    Provides some stats about Nginx logs stored in MongoDB.
+    Args:
+        mongo_collection: The PyMongo collection object.
+    Returns:
+        None
+    """
+    total_logs = mongo_collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = mongo_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_check = mongo_collection.count_documents({"method": "GET",
+                                                     "path": "/status"})
+    print(f"{status_check} status check")
+
+    # Top 10 most present IPs
+    pipeline = [
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ]
+    top_ips = list(mongo_collection.aggregate(pipeline))
+    print("IPs:")
+    for ip in top_ips:
+        print(f"\t{ip['_id']}: {ip['count']}")
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    collection = db.nginx
+    log_stats(collection)
